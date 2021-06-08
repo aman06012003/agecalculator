7102681266abe3b8e2444105ee5468c0c4463904
Aman Kapoor <akalasklo@gmail.com>
2021-06-09 01:23:05 +0530

    added some new modifications

--- age.py
+++ age.py
@@ -1,3 +1,9 @@
-year = int(input('Enter your birth year)
-age = 2021 - year
+birthyear = int(input('Enter your birth year: '))
+age = 2021-birthyear
+if age >= 18:
+    print('You are adult')
+elif 12 < age < 18:
+    print('You are teenage')
+else:
+    print('You are a cute kid')
 print(age)
